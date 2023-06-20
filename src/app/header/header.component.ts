db3c90d48dc80a7e123c4cecfea02bb4c473a34b
jay <barotjay998>
2023-06-19 23:03:15 -0500

    experience section complete

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,10 +8,30 @@ import { Component, HostListener, OnInit } from '@angular/core';
 export class HeaderComponent {
 
   isNavbarTransparent = true;
+  isTogglerClicked = false;
 
   @HostListener('window:scroll')
   onWindowScroll() {
-    this.isNavbarTransparent = (window.pageYOffset <= 50);
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    if ((scrollPosition > 50) || (this.isTogglerClicked)) {
+      this.isNavbarTransparent = false;
+    }
+    else {
+      this.isNavbarTransparent = true;
+    }
+    
+  }
+
+  togglerClicked(): void {
+    this.isTogglerClicked = !this.isTogglerClicked;
+    
+    if (this.isTogglerClicked == true) {
+      this.isNavbarTransparent = false;
+    }
+    else {
+      this.isNavbarTransparent = true;
+    }
   }
 
   ngOnInit() {

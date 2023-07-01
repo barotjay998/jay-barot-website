e1189dbf550ecfb43e5c377c83c468031574f78e
jay <barotjay998>
2023-06-30 23:08:05 -0500

    projects-done

---
+++ src/app/services/data-service.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError} from 'rxjs';
+import { catchError} from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataServiceService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getProjectData(): Observable<any>  {
+
+    var url = `${environment.api_url}/project-data.json`;
+
+    return this.http.get(url)
+      .pipe(catchError(this.handleError));
+  }
+
+  handleError(error: HttpErrorResponse) {
+    return throwError(error.message || "Server Error");
+  }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    // api_url: 'https://129.114.27.48/v1/api'
+    api_url: 'assets'
+};
+  
